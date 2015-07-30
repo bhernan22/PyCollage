19ac3c1f2f81aeb577b47e94ace47bbda5cc5e01
Martin Almaraz <martin.almaraz@gmail.com>
2015-07-30 14:32:36 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from PIL import Image
+
+HOR = 25
+VER = 25
+
+
+def main():
+    im = Image.open("test.jpg")
+    width, height = im.size
+    print width, height
+    cutBox(im)
+
+def cutBox(im):
+    startX = 0
+    startY = 0
+    box = (startX, startY, HOR, VER)
+    region = im.crop(box)
+    findAverageColor(region)
+
+def findAverageColor(im):
+    red = 0
+    green = 0
+    blue = 0
+
+    x = 0
+    y = 0
+
+    pixels = im.load()
+    
+    while(x < )
+
+main()
